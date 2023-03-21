f2586f0332a58e2ec0347326c3818843cf3c8e23
Aishik Pal <aishikpal.sp@gmail.com>
2023-03-21 23:46:48 +0530

    Commits on 21/03/23.

---
+++ AccountDetails/Account.cs
@@ -0,0 +1,74 @@
+﻿namespace AccountDetails
+{
+    public class Account
+    {
+        // class members
+        private int id;
+        private string accountType;
+        double balance;
+
+        // class properties
+        public int ID 
+        { 
+            get
+            {
+                return id;
+            } 
+            set
+            {
+                id = value;
+            } 
+        }
+        public string AccountType 
+        { get 
+            {  
+                return accountType; 
+            } 
+            set 
+            { 
+                accountType = value; 
+            } 
+        }
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+
+        // class constructors
+        public Account()
+        {
+
+        }
+        public Account(int id, string accountType, double balance)
+        {
+            ID = id;
+            AccountType = accountType;
+            Balance = balance;
+        }
+
+        // class methods
+        //double newBalance = 0;
+        public bool WithDraw(double amount)
+        {
+            if (Balance > amount)
+            {
+                Balance = Balance - amount;
+                return true;
+            }
+            else
+                return false;
+        }
+        public string GetDetails()
+        {
+            string result = "Account Id: " + ID + "\nAccount Type: " + AccountType + "\nBalance: " + Balance;
+            return result;
+        }
+    }
+}
---
+++ AccountDetails/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AccountDetails
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account id");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter account type");
+            string accountType = Console.ReadLine();
+            Console.WriteLine("Enter account balance");
+            double balance = double.Parse(Console.ReadLine());
+            Account account = new Account(id, accountType, balance);
+            Console.WriteLine("Enter amount to withdraw");
+            double amount = double.Parse(Console.ReadLine());
+            Console.WriteLine(account.GetDetails());
+            if (account.WithDraw(amount))
+                Console.WriteLine("New Balance: " + account.Balance);
+        }
+    }
+}
