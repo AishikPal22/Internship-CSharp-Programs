c2c92cac8a2604ac60dbdf605abb11bb718e2f8b
Aishik Pal <aishikpal.sp@gmail.com>
2023-03-27 22:26:12 +0530

    CC2 - 27/03/23

---
+++ Code Challenge - InsuranceDetails/MotorInsurance.cs
@@ -0,0 +1,38 @@
+﻿namespace InsuranceDetails
+{
+    public class MotorInsurance : Insurance
+    {
+        // members
+        private double idv;
+        private float depPercent;
+
+        // properties
+        public double Idv
+        {
+            get { return idv; } 
+            set {  idv = value; }
+        }
+        public float DepPercent
+        {
+            get { return depPercent; }
+            set { depPercent = value; }
+        }
+
+        // constructors
+        public MotorInsurance() { }
+        public MotorInsurance(string insuranceNo, string insuranceName, double amountCovered, float depPercent) : base (insuranceNo, insuranceName, amountCovered)
+        {
+            InsuranceNo = insuranceNo;
+            InsuranceName = insuranceName;
+            AmountCovered = amountCovered;
+            DepPercent = depPercent;
+        }
+
+        // methods
+        public override double calculatePremium()
+        {
+            Idv = AmountCovered - (AmountCovered * DepPercent / 100);
+            return 0.03 * Idv;
+        }
+    }
+}
---
+++ Code Challenge - InsuranceDetails/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace InsuranceDetails
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program pr = new Program();
+
+            Console.WriteLine("Insurance Number : ");
+            string iNo = Console.ReadLine();
+            Console.WriteLine("Insurance Name : ");
+            string iName = Console.ReadLine();
+            Console.WriteLine("Amount Covered : ");
+            double amount = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select\n1.Life Insurance\n2.Motor Insurance");
+            int option = int.Parse(Console.ReadLine());
+
+            if (option == 1)
+            {
+                Console.WriteLine("Policy Term : ");
+                int policy = int.Parse(Console.ReadLine());
+                Console.WriteLine("Benefit Percent : ");
+                float benefit = float.Parse(Console.ReadLine());
+
+                Insurance li = new LifeInsurance(iNo, iName, amount, policy, benefit);
+                double result = pr.addPolicy(li, option);
+                Console.WriteLine($"Calculated Premium : {result}");
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine("Depreciation Percent : ");
+                float dep = float.Parse(Console.ReadLine());
+
+                Insurance mi = new MotorInsurance(iNo, iName, amount, dep);
+                double result = pr.addPolicy(mi, option);
+                Console.WriteLine($"Calculated Premium : {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Option.");
+            }
+        }
+
+        public double addPolicy(Insurance ins, int opt)
+        {
+            // return ins.calculatePremium(); // this was sufficient.
+
+            if (opt == 1)
+            {
+                return ins.calculatePremium();
+            }
+            if (opt == 2)
+            {
+                return ins.calculatePremium();
+            }
+            return 0;
+        }
+    }
+}
